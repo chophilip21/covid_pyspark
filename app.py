bf657945b99f6db15e97f0e213a04b76cf6ef54f
USER <USER@sfu.ca>
2021-01-05 15:26:19 -0800

    bootstrap template complete

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ findspark.find()
 # os.environ['JAVA_HOME'] = '/Library/Java/JavaVirtualMachines/jdk1.8.0_271.jdk/Contents/Home'
 
 @app.route('/', methods=['POST', 'GET'])
-def bar():
+def cumulative():
 
     spark = SparkSession.builder.appName('covid_19_analysis').getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
@@ -49,6 +49,37 @@ def bar():
 
     return render_template('cumulative.html',
                            title=f'Canadian cumulative statistics up to {df.date[1]}', 
+                           labels=bar_labels, values_default=cumulative_cases,
+                           values_active_cases = active_cases, values_cumulative_cases = cumulative_cases,
+                           values_cumulative_tested = cumulative_tested, values_cumulative_deaths= cumulative_deaths,
+                           values_vaccine = vaccine_administration, values_cumulative_recovered = cumulative_recovered 
+                           )
+
+@app.route('/forecast', methods=['POST', 'GET'])
+def timeseries():
+
+    spark = SparkSession.builder.appName('covid_19_analysis').getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+
+    static_data = data_to_df('cumulative', spark)
+
+    df = static_data.toPandas()
+    print(df.head(15))
+
+    # label
+    bar_labels = df.province 
+
+    # values
+    active_cases = df.active_cases
+    cumulative_cases = df.cumulative_cases # default
+    cumulative_tested = df.cumulative_tested
+    cumulative_deaths = df.cumulative_deaths
+    vaccine_administration = df.vaccine_administration
+    cumulative_recovered = df.cumulative_recovered
+
+
+    return render_template('timeseries.html',
+                           title=f'Time series stats up to {df.date[1]}', 
                            labels=bar_labels, values_default=cumulative_cases,
                            values_active_cases = active_cases, values_cumulative_cases = cumulative_cases,
                            values_cumulative_tested = cumulative_tested, values_cumulative_deaths= cumulative_deaths,
